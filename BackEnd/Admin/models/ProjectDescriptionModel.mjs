2fc671872db0b693bf61d55283f538b9dcad139d
HaiderF21 <f219409@cfd.nu.edu.pk>
2024-05-06 16:33:25 +0500

    Project Description Schema and Admin Utilization with Project Descrition Schema

--- BackEnd/Admin/models/ProjectDescriptionModel.mjs
+++ BackEnd/Admin/models/ProjectDescriptionModel.mjs
@@ -5,9 +5,13 @@ const ProjectDescriptionModel = new mongoose.Schema(
     ProjectName:String,
     ProjectDescription:String,
     User_ID : Number,
+    Student_ID:Number,
+    proposal_ID,Number,
     Company_ID : Number,
     Approval : Boolean,
-    Admin_Approval_ID: Number
+    Admin_Approval_ID: Number,
+    Category:String,
+    ProjectSubmissionDate:Date
 }
 );
 
--- BackEnd/Admin/routes/adminroutes.mjs
+++ BackEnd/Admin/routes/adminroutes.mjs
@@ -1,13 +1,38 @@
 import express from "express";
 import AdminModel from "../models/AdminModel.mjs";
-
+import bcrypt from "bcrypt";
 const routes = express.Router();
+
+const saltRounds = 10;
+const hashPassword = async(password)=>{
+    return await bcrypt.hash(password,saltRounds);
+} 
 
 routes.get('/',async(req,res)=>{
     
     res.send(await AdminModel.find({}));
 });
 
+routes.post('/SignUp', async (req, res) => {
+    const { Username, Password, Email, Name, Age } = req.body;
+    try {
+      const hashedPassword = await hashPassword(Password);
+      const user = new AdminModel({
+        Username: Username,
+        Password: hashedPassword, // Store hashed password in the database
+        Email: Email,
+        Name: Name,
+        Age: Age
+      });
+      const dataSave = await user.save();
+      res.status(200).json(dataSave);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
+
+/*
 routes.post('/SignUp',async(req,res)=>
 {
     const {Username,Password,Email,Name,Age} = req.body;
@@ -30,12 +55,13 @@ routes.post('/SignUp',async(req,res)=>
         res.status(400).json({message:e.message});
     }
 });
-
+*/
 routes.post('/login',async(req,res)=>
 {
     const {Email,Password} = req.body;
+    const password = await hashPassword(Password);
     try{
-    const result = await AdminModel.findOne({Email:Email,Password:Password});
+    const result = await AdminModel.findOne({Email:Email,Password:password});
     res.status(200).json(result);   
 }
 catch(e)
